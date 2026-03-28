f6926ce782c654b2e85e877c5e6ee4380e94cab6
Diego Fernando Aparicio Castro <dapariciocastro13@gmail.com>
2026-03-27 21:38:42 -0500

    Inicio de tarea #17

---
+++ Num-Back/middlewares/passwordValidator.js
@@ -0,0 +1,5 @@
+/**
+ * Middleware para validar la fortaleza de la contraseña según HUS-02 (C2).
+ * Debe cumplir: mín 8 caracteres, 1 mayúscula, 1 número y 1 carácter especial.
+ */
+// Lógica de validación por Regex aquí
